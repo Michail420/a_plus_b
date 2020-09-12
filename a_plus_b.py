7b4f41832eb036deb62b0f2039d353b58a41ea09
Michail420 <71172056+Michail420@users.noreply.github.com>
2020-09-12 14:44:30 +0500

    Add files via upload

---
+++ a_plus_b.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 21:33:24 2020
+
+@author: user
+"""
+a = float(input('Введите первое число '))
+b = float(input('Введите второе число '))
+print('Сумма равна ', (a+b))
+
